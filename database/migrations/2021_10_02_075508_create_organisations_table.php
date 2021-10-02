616528f33d0908c289da1e91450bf94f79a92495
Marc Hampson <mhampson@wk360.com>
2021-10-02 09:59:07 +0100

    Initial commit with user->organisation relationship

---
+++ database/migrations/2021_10_02_075508_create_organisations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganisationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organisations', function (Blueprint $table) {
+            $table->id();
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->string('postcode')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organisations');
+    }
+}
